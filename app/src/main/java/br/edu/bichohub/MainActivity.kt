8874cb8ca3566205ba5465ece461101a13cdddc3
bolachamole <atismistic@proton.me>
2026-05-17 23:07:20 -0400

    Template para as Activities futuras. Closes #1

--- app/src/main/java/br/edu/bichohub/MainActivity.kt
+++ app/src/main/java/br/edu/bichohub/MainActivity.kt
@@ -4,14 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import br.edu.bichohub.ui.theme.BichoHubTheme
+import br.edu.bichohub.ui.theme.Template
 
 /**
  * Activity principal do aplicativo, ativada ao inicializá-lo.
@@ -22,29 +19,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             BichoHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Template("BichoHub", content= {
+                    Row(modifier = Modifier.fillMaxSize()) { }
+                })
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BichoHubTheme {
-        Greeting("Android")
     }
 }
